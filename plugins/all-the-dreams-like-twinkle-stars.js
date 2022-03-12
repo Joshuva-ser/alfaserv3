d3365657675d145ba58394e4cc74c2c2f2982eb0
Joshuva-ser <96343347+Joshuva-ser@users.noreply.github.com>
2022-03-12 18:28:17 +0530

    Update all-the-dreams-like-twinkle-stars.js

--- plugins/all-the-dreams-like-twinkle-stars.js
+++ plugins/all-the-dreams-like-twinkle-stars.js
@@ -1,8 +1,8 @@
-/* Amalser Bot
-Re-edit Amalser
+/* Amalser 
+Re-edit Joshuva-ser
 */
 
-const Amalser = require('../events');
+const Alfaserv3 = require('../events');   
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
@@ -13,20 +13,23 @@ const os = require('os');
 var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
-
+var sk1,sk2
+var split = Config.Alfaserv3.split('/');
+         sk2 = split[1];
+         sk1 = split[0];
 
 let whb = Config.WORKTYPE == 'public' ? false : true
 
-Amalser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
-// send a buttons message!credit Amal
+Alfaser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+// send a buttons message!credit Ajayan
     const buttons = [
-        {buttonId: 'id1', buttonText: {displayText: Config.ALIVE_BUTTON }, type: 1},
-        {buttonId: 'id2', buttonText: {displayText: Config.BUTTON_TWO }, type: 1}
+        {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: sk2 }, type: 1}
       ]
       
       const buttonMessage = {
           contentText: '```'+Config.BOT+'\n\n```'+Config.ALIVEMSG+'\n',
-          footerText: 'Amalser ©',
+          footerText: 'ᴊᴏꜱʜᴜᴠᴀ',
           buttons: buttons,
           headerType: 1
       }
